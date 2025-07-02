cadcf3a2118e0abca30f395219b6e86ceb5d3ecd
David Kwong <20544dk@gmail.com>
2025-07-02 23:36:59 +1000

    Better explanation

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -8,7 +8,7 @@ import (
 // Super contrived authentication system.
 // Currently there is only an api key, which the auth middleware checks.
 // If the request passes the middleware then we return that the user is authenticated
-// When I have more time, this will return a properly formed JWT.
+// When I have more time, this will removed in favor of a proper authentication system with signed JWTs
 func (s *Server) isAuthenicated(w http.ResponseWriter, r *http.Request) {
 	handlerutil.Json(w, true)
 }
